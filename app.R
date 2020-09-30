0406f70be254a701d5b8b49f26943e7c27b22e95
Heili Lowman <heilil@sccwrp.org>
2020-09-30 10:19:07 -0700

    First text output working (linked app & RMarkdown).

--- app.R
+++ app.R
@@ -1,4 +1,58 @@
-# Arid Southwest Method Development
+# Arid SouthWest Method Development
 # Shiny Application for pdf Report Generation
 # Created by: Heili Lowman
 # Created on: September 30, 2020
+
+
+# Setup -------------------------------------------------------------------
+
+# Create Shiny app. Anything in the sections below (user interface & server) should be the reactive/interactive parts of the shiny application.
+
+# User Interface ----------------------------------------------------------
+
+shinyApp(
+  ui = fluidPage(
+    
+    # App title
+    titlePanel("Field form for the Arid West Streamflow Duration Assessment Method"),
+    
+    # Title panel subtext
+    tags$div(
+      "This is a draft tool to calculate the interim Streamflow Duration Assessment Method (SDAM) developed for the Arid West region. Do not use for regulatory purposes without prior consulting with the EPA product delivery team. Contact Raphael Mazor (raphaelm@sccwrp.org) with questions."),
+    
+    textInput("project", label = h3("Project Name or Number"), value = "Enter text..."), # text input box
+    
+    hr(), # adds divider
+    
+    downloadButton("report", "Generate report")
+  ),
+  
+
+# Server ------------------------------------------------------------------
+
+  server = function(input, output) {
+    output$report <- downloadHandler(
+      filename = "report.pdf",
+      content = function(file) {
+        # Copy the report file to a temporary directory before processing it, in
+        # case we don't have write permissions to the current working dir (which
+        # can happen when deployed).
+        tempReport <- file.path(tempdir(), "report.Rmd")
+        file.copy("report.Rmd", tempReport, overwrite = TRUE)
+        
+        # Set up parameters to pass to Rmd document
+        params <- list(n = input$project)
+        
+        # Knit the document, passing in the `params` list, and eval it in a
+        # child of the global environment (this isolates the code in the document
+        # from the code in this app).
+        rmarkdown::render(tempReport, output_file = file,
+          params = params,
+          envir = new.env(parent = globalenv())
+        )
+      }
+    )
+  }
+)
+
+# End of R Shiny app script.
